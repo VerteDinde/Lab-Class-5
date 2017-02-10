68fb4119aaa86f63d27c49617ebb974b427011a8
Keeley Hammond <keeleymhammond@gmail.com>
2017-02-10 15:17:28 -0800

    Function five completed

--- app.js
+++ app.js
@@ -109,22 +109,28 @@ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
-// /////////////////////////////////////
-// /* Problem 5
-// Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+/////////////////////////////////////
+/* Problem 5
+Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
-// "The numbers 2,3,4 have a product of 24."
+"The numbers 2,3,4 have a product of 24."
 
-// IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 
-// Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
-// // Write your code here
-// function multiplyArray(testArray){ //eslint-disable-line
+// Write your code here
+function multiplyArray(testArray) { //eslint-disable-line
+  var arrayProduct = 0;
+  arrayProduct = multiply(testArray[0], multiply(testArray[1], testArray[2])[0])[0];
 
-// }
+  var message = 'The numbers ' + testArray + ' have a product of ' + arrayProduct + '.';
+  console.log(message);   // debugging
 
-// // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+  return [arrayProduct, message];
+}
 
-// // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+// Here is the test for multiplyArray(); uncomment it to run it
+testMultiplyArray(2, 3, 4);
+
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
